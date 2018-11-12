9717f32088253f3d12bfa6fc690d23a5b5e14ec9
johnwayodi <wayodijohn@gmail.com>
2018-11-12 23:59:59 +0300

    [Feature #161865937] Add logout functionality

--- js/app.js
+++ js/app.js
@@ -834,3 +834,55 @@ function createSaleDetailItem(saleRecordItem) {
     itemRow.appendChild(itemCost);
     return itemRow;
 }
+
+function logoutUser() {
+    document.getElementById("login-button").disabled = true;
+    let myForm = document.getElementById("form1");
+
+    let uName = myForm.elements["username"].value;
+    let uPass = myForm.elements["password"].value;
+    let dashboard = undefined;
+    fetch(url + '/auth/login', {
+        method: 'post',
+        headers: {
+            "Content-type": "application/json; charset=utf-8"
+        },
+        body: JSON.stringify({
+            username: uName,
+            password: uPass
+        })
+    }).then(function (response) {
+        if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+                response.status);
+        }
+        return response.json()
+    }).then(function (data) {
+        let message = data.message;
+        if (message === 'login successful') {
+            let role = data['user_role'];
+            let token = data['access_token'];
+            localStorage.setItem('userrole', role);
+            localStorage.setItem('authtoken', token);
+            console.log(token);
+            console.log(role);
+            if (role === 'admin') {
+                dashboard = "dashboard-admin.html";
+            }
+            if (role === 'attendant') {
+                dashboard = "dashboard-attendant.html"
+            }
+            myForm.reset();
+            showSnackBar(message);
+
+            setTimeout(function () {
+                openNextPage(dashboard)
+            }, 3300);
+        }
+        else {
+            showSnackBar(data['message'], "error");
+            document.getElementById("login-button").disabled = false;
+            console.log(message);
+        }
+    }).catch(err => console.log(err))
+}
